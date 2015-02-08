9e96e8e0399ccff01ae3461bc6318128186d44c7
Jorge Eremiev <jorgech3@gmail.com>
2015-02-08 18:45:36 -0500

    Tweaks for memory usage.

--- config.h
+++ config.h
@@ -117,7 +117,7 @@
 #define __POOL_100B_SIZE 	(__BLOCK_100B * 64)
 #define __POOL_80B_SIZE 	(__BLOCK_80B * 48)
 #define __POOL_48B_SIZE 	(__BLOCK_48B * 32)
-#define __POOL_32B_SIZE 	(__BLOCK_32B * 64)
+#define __POOL_32B_SIZE 	(__BLOCK_32B * 128)
 
 #ifndef __DEBUG_TOOLS
 #define __POOL_28B_SIZE 	(__BLOCK_28B * 512)
