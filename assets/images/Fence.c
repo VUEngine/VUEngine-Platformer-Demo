4ad5260f1154540a3c905eb9785a716740dde9a7
Chris <chris@gamiac.org>
2015-05-02 16:53:24 +0200

    Fixed fence graphic

--- assets/images/Fence.c
+++ assets/images/Fence.c
@@ -4,26 +4,27 @@
 //======================================================================
 //
 //	Fence, 24x16@2, 
-//	+ 5 tiles (t|f reduced) not compressed
+//	+ 7 tiles (t reduced) not compressed
 //	+ regular map (flat), not compressed, 3x2 
-//	Total size: 80 + 12 = 92
+//	Total size: 112 + 12 = 124
 //
-//	Time-stamp: 2015-04-30, 19:17:55
+//	Time-stamp: 2015-05-02, 16:25:02
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
 //======================================================================
 
-const unsigned int FenceTiles[20] __attribute__((aligned(4)))=
+const unsigned int FenceTiles[28] __attribute__((aligned(4)))=
 {
 	0x00000000,0x00000000,0x00000000,0x00000000,0x07D00140,0xEAA45EB4,0x9AA49AA4,0x1AA45AA4,
-	0x00000000,0xFFFF5555,0xAAAAAAAA,0x00005555,0xEAA45AA4,0x9AA49AA4,0x1AA45AA4,0x1AA41AA4,
-	0xFFFF5555,0xAAAAAAAA,0x00005555,0x00000000,
+	0x00000000,0xFFFF5555,0xAAAAAAAA,0x00005555,0x07D00140,0x1AAB1EB5,0x1AA61AA6,0x1AA41AA5,
+	0xEAA45AA4,0x9AA49AA4,0x1AA45AA4,0x1AA41AA4,0xFFFF5555,0xAAAAAAAA,0x00005555,0x00000000,
+	0x1AAB1AA5,0x1AA61AA6,0x1AA41AA5,0x1AA41AA4,
 };
 
 const unsigned short FenceMap[6] __attribute__((aligned(4)))=
 {
-	0x0001,0x0002,0x2001,0x0003,0x0004,0x2003,
+	0x0001,0x0002,0x0003,0x0004,0x0005,0x0006,
 };
 
 //}}BLOCK(Fence)
--- source/objects/backgrounds/FenceDefinition.c
+++ source/objects/backgrounds/FenceDefinition.c
@@ -46,7 +46,7 @@ TextureROMDef FENCE_TX =
         // __ANIMATED_MULTI: sum of chars of all animation frames
         // __ANIMATED_SHARED: number of chars of a single animation frame (cols * rows of this texture)
         // __NOT_ANIMATED: number of chars of whole image
-        4,
+        7,
 
         // allocation type
         __NOT_ANIMATED,
