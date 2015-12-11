e178ac5e4c2267c4ff71542a699e03785a19aec6
Chris <chris@vr32.de>
2015-12-11 12:29:53 +0100

    John Hero is dead, long live the Ninja Wiener!

--- assets/images/GUI/GUIBlank.c
+++ assets/images/GUI/GUIBlank.c
@@ -8,7 +8,7 @@
 //	+ regular map (flat), not compressed, 48x2 
 //	Total size: 32 + 192 = 224
 //
-//	Time-stamp: 2015-11-29, 18:16:28
+//	Time-stamp: 2015-12-11, 12:26:46
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
