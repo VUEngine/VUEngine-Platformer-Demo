14592ab84889197ab4f457d5b6fc78002cd05120
Chris <chris@vr32.de>
2016-01-12 22:07:13 +0100

    Added "under construction" sign to tower

--- source/objects/backgrounds/Level-1/Tower/Level-1-Tower-Back-1.c
+++ source/objects/backgrounds/Level-1/Tower/Level-1-Tower-Back-1.c
@@ -43,7 +43,7 @@ CharSetROMDef LEVEL_1_TOWER_BACK_1_CH =
     // __ANIMATED_SHARED_COORDINATED: number of chars of a single animation frame (cols * rows)
     // __ANIMATED_MULTI: sum of chars of all animation frames
     // __NOT_ANIMATED: number of chars of whole image
-    50,
+    84,
 
     // allocation type
     __NOT_ANIMATED,
