9cc27bd6a66d12ae8f90ca1f6ed7b58dac9a756d
Jorge Eremiev <jorgech3@gmail.com>
2016-09-22 23:31:53 -0500

    Made snail to use a common animation coordinator.

--- source/objects/actors/Snail/SnailDefinition.c
+++ source/objects/actors/Snail/SnailDefinition.c
@@ -72,11 +72,11 @@ CharSetROMDef SNAIL_CH =
     // number of chars, depending on allocation type:
     // __ANIMATED_SINGLE, _SHARED, _SHARED_COORDINATED: number of chars of a single animation frame (cols * rows)
     // __ANIMATED_MULTI, __NOT_ANIMATED: sum of all chars
-    18,
+    6,
 
     // allocation type
     // (__ANIMATED_SINGLE, __ANIMATED_SHARED, __ANIMATED_SHARED_COORDINATED, __ANIMATED_MULTI or __NOT_ANIMATED)
-    __ANIMATED_MULTI,
+    __ANIMATED_SHARED_COORDINATED,
 
     // char definition
     SnailTiles,
