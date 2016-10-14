86a1335e810099b0f7c01dc2703436727e1655c1
Christian Radke <chris@vr32.de>
2016-10-14 23:23:08 +0200

    Slightly rearranged AdjustmentScreen

--- source/screens/SplashScreens/AdjustmentScreen/Definition/AdjustmentScreenBGDefinition.c
+++ source/screens/SplashScreens/AdjustmentScreen/Definition/AdjustmentScreenBGDefinition.c
@@ -39,7 +39,7 @@ CharSetROMDef ADJUSTMENT_SCREEN_BG_CH =
     // number of chars, depending on allocation type:
     // __ANIMATED_SINGLE, _SHARED, _SHARED_COORDINATED: number of chars of a single animation frame (cols * rows)
     // __ANIMATED_MULTI, __NOT_ANIMATED: sum of all chars
-    32,
+    37,
 
     // allocation type
     // (__ANIMATED_SINGLE, __ANIMATED_SHARED, __ANIMATED_SHARED_COORDINATED, __ANIMATED_MULTI or __NOT_ANIMATED)
