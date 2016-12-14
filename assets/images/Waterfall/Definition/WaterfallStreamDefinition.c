b0800aab48c9cb545dc8a28c6c66045c373213e5
Jorge Eremiev <jorgech3@gmail.com>
2016-12-14 16:44:36 -0500

    Merge.

--- assets/images/Waterfall/Definition/WaterfallStreamDefinition.c
+++ assets/images/Waterfall/Definition/WaterfallStreamDefinition.c
@@ -20,7 +20,7 @@
 //---------------------------------------------------------------------------------------------------------
 
 #include <libgccvb.h>
-#include <TransparentAnimatedInGameEntity.h>
+#include <AnimatedInGameEntity.h>
 #include <BgmapAnimatedSprite.h>
 #include <macros.h>
 
@@ -119,6 +119,9 @@ BgmapSpriteROMDef WATERFALL_STREAM_SPRITE =
         // texture definition
         (TextureDefinition*)&WATERFALL_STREAM_TX,
 
+        // transparent
+        true,
+
         // displacement
         {
         	0,
@@ -144,7 +147,7 @@ AnimatedInGameEntityROMDef WATERFALL_STREAM_AG =
 {
     {
         {
-            __TYPE(TransparentAnimatedInGameEntity),
+            __TYPE(AnimatedInGameEntity),
             (SpriteROMDef**)WATERFALL_STREAM_SPRITES,
         },
 
