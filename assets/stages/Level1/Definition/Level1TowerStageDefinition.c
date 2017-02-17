646d5c39e1248ce10c23768582ce7529ee58546e
Jorge Eremiev <jorgech3@gmail.com>
2017-02-16 20:55:34 -0500

    Tweaked the streaming.

--- assets/stages/Level1/Definition/Level1TowerStageDefinition.c
+++ assets/stages/Level1/Definition/Level1TowerStageDefinition.c
@@ -341,7 +341,7 @@ StageROMDef LEVEL_1_TOWER_STAGE_ST =
 	// streaming
 	{
 		// minimum free ms in the current game frame to allow streaming to quick in
-		5,
+		4,
 		// load padding
 		120,
 		// unload padding
