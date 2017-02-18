0c47b021fb74676ba9b67274717964602b21098b
Christian Radke <chris@vr32.de>
2017-02-18 21:47:54 +0100

    Mini fix in Overworld image

--- assets/images/Stages/Overworld/Binary/FlowerA.c
+++ assets/images/Stages/Overworld/Binary/FlowerA.c
@@ -8,7 +8,6 @@
 //	+ regular map (flat), not compressed, 2x2 
 //	Total size: 80 + 8 = 88
 //
-//	Time-stamp: 2016-12-05, 20:35:49
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
--- assets/images/Stages/Overworld/Definition/Overworld1Definition.c
+++ assets/images/Stages/Overworld/Definition/Overworld1Definition.c
@@ -33,7 +33,7 @@
 //												DECLARATIONS
 //---------------------------------------------------------------------------------------------------------
 
-extern BYTE OverworldTiles[];
+extern BYTE Overworld_1Tiles[];
 extern BYTE Overworld_1Map[];
 
 
@@ -46,14 +46,14 @@ CharSetROMDef OVERWORLD_CH =
 	// number of chars, depending on allocation type:
 	// __ANIMATED_SINGLE, _SHARED, _SHARED_COORDINATED: number of chars of a single animation frame (cols * rows)
 	// __ANIMATED_MULTI, __NOT_ANIMATED: sum of all chars
-	332,
+	330,
 
 	// allocation type
 	// (__ANIMATED_SINGLE, __ANIMATED_SHARED, __ANIMATED_SHARED_COORDINATED, __ANIMATED_MULTI or __NOT_ANIMATED)
 	__NOT_ANIMATED,
 
 	// char definition
-	OverworldTiles,
+	Overworld_1Tiles,
 };
 
 TextureROMDef OVERWORLD_1_TX =
