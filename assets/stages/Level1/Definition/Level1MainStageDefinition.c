d1d0b41d6a0dd0a7bbfdc0ee9e34cf2219d9fbf8
Jorge Eremiev <jorgech3@gmail.com>
2017-03-16 17:46:43 -0500

    Tweaked texture writing deferring value to take advantage of the new Mem's assembly based functions and reduce visible texture drawn-in.

--- assets/stages/Level1/Definition/Level1MainStageDefinition.c
+++ assets/stages/Level1/Definition/Level1MainStageDefinition.c
@@ -633,7 +633,7 @@ StageROMDef LEVEL_1_MAIN_STAGE_ST =
 		1,
 
 		// maximum number of texture's rows to write each time the texture writing is active
-		5,
+		10,
 
 		// maximum number of rows to compute on each call to the affine functions
 		24,
