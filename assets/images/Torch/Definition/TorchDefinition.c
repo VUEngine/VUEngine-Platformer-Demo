bd980a11f4275b0f6f55ae9dc08acb1e0668188a
Jorge Eremiev <jorgech3@gmail.com>
2017-04-27 21:38:23 -0500

    Added off torch.

--- assets/images/Torch/Definition/TorchDefinition.c
+++ assets/images/Torch/Definition/TorchDefinition.c
@@ -27,6 +27,7 @@
 #include <libgccvb.h>
 #include <AnimatedInGameEntity.h>
 #include <ObjectAnimatedSprite.h>
+#include <StaticImage.h>
 #include <macros.h>
 
 
@@ -177,3 +178,81 @@ AnimatedInGameEntityROMDef TORCH_AG =
 	// initial animation
 	"Burn",
 };
+
+CharSetROMDef TORCH_OFF_CH =
+{
+	// number of chars, depending on allocation type:
+	// __ANIMATED_SINGLE*, __ANIMATED_SHARED*: number of chars of a single animation frame (cols * rows)
+	// __ANIMATED_MULTI, __NOT_ANIMATED: sum of all chars
+	3,
+
+	// allocation type
+	// (__ANIMATED_SINGLE, __ANIMATED_SINGLE_OPTIMIZED, __ANIMATED_SHARED, __ANIMATED_SHARED_COORDINATED, __ANIMATED_MULTI or __NOT_ANIMATED)
+	__NOT_ANIMATED,
+
+	// char definition
+	TorchTiles,
+};
+
+TextureROMDef TORCH_OFF_TX =
+{
+	// charset definition
+	(CharSetDefinition*)&TORCH_OFF_CH,
+
+	// bgmap definition
+	TorchMap,
+
+	// cols (max 64)
+	1,
+
+	// rows (max 64)
+	3,
+
+	// padding for affine/hbias transformations (cols, rows)
+	{0, 0},
+
+	// number of frames, depending on charset's allocation type:
+	// __ANIMATED_SINGLE*, __ANIMATED_SHARED*, __NOT_ANIMATED: 1
+	// __ANIMATED_MULTI: total number of frames
+	1,
+
+	// palette number (0-3)
+	0,
+};
+
+ObjectSpriteROMDef TORCH_OFF_SPRITE =
+{
+	{
+		// sprite's type
+		__TYPE(ObjectAnimatedSprite),
+
+		// texture definition
+		(TextureDefinition*)&TORCH_OFF_TX,
+
+		// transparent
+		false,
+
+		// displacement
+		{0, 0, FTOFIX19_13(1), 0},
+	},
+
+	// bgmap mode (__WORLD_BGMAP, __WORLD_AFFINE, __WORLD_OBJECT or __WORLD_HBIAS)
+	// make sure to use the proper corresponding sprite type throughout the definition (BgmapSprite or ObjectSprite)
+	__WORLD_OBJECT,
+
+	// display mode (__WORLD_ON, __WORLD_LON or __WORLD_RON)
+	__WORLD_ON,
+};
+
+ObjectSpriteROMDef* const TORCH_OFF_SPRITES[] =
+{
+	&TORCH_OFF_SPRITE,
+	NULL
+};
+
+StaticImageROMDef TORCH_OFF_IM =
+{
+	__TYPE(StaticImage),
+	(SpriteROMDef**)TORCH_OFF_SPRITES,
+};
+
