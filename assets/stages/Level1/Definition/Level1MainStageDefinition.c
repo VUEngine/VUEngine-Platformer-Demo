5331abe51466a1c8f72615f92767ae3fe9ad3231
Jorge Eremiev <jorgech3@gmail.com>
2017-05-14 14:42:57 -0500

    Put back raining.

--- assets/stages/Level1/Definition/Level1MainStageDefinition.c
+++ assets/stages/Level1/Definition/Level1MainStageDefinition.c
@@ -589,7 +589,7 @@ StageTextureEntryROMDef LEVEL_1_MAIN_STAGE_ST_TEXTURES[] =
 
 const PostProcessingEffect LEVEL_1_MAIN_STAGE_ST_POST_PROCESSING_EFFECTS[] =
 {
-//	PostProcessingEffects_rain,
+	PostProcessingEffects_rain,
 	NULL
 };
 
