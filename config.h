2f312139d9396a08eb9814907f1a79b8e688efa7
Jorge Eremiev <jorgech3@gmail.com>
2017-08-08 15:51:40 -0500

    Minor performance tweaks.

--- config.h
+++ config.h
@@ -117,7 +117,8 @@
 #define __VERTICAL_VIEW_POINT_CENTER			__SCREEN_HEIGHT / 2
 
 // Parallax values are divide by this factor to control their strength
-#define __PARALLAX_CORRECTION_FACTOR			16
+// powers of 2
+#define __PARALLAX_CORRECTION_FACTOR			4
 
 
 //---------------------------------------------------------------------------------------------------------
