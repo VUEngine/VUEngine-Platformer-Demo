dc82ce5c5e1c58adb9c50c0e6dd55a9ef45ec077
Jorge Eremiev <jorgech3@gmail.com>
2017-11-23 00:58:37 -0500

    Increased the size on the Z axis of collision to avoid other entities to be pushed over that axis upon collision. Need to rename the collisions sizes.

--- source/objects/elements/Collision/Collision.c
+++ source/objects/elements/Collision/Collision.c
@@ -38,43 +38,43 @@
 //												DEFINITIONS
 //---------------------------------------------------------------------------------------------------------
 
-const Size collision_2_28_1 = 	{2 * 8, 	28 * 8, 	1 * 8};
-const Size collision_2_64_1 = 	{2 * 8, 	64 * 8, 	1 * 8};
-const Size collision_3_2_1 = 	{3 * 8, 	2 * 8, 		1 * 8};
-const Size collision_4_2_1 = 	{4 * 8, 	2 * 8, 		1 * 8};
-const Size collision_4_4_1 = 	{4 * 8, 	4 * 8, 		1 * 8};
-const Size collision_4_8_1 = 	{4 * 8, 	8 * 8, 		1 * 8};
-const Size collision_6_4_1 = 	{6 * 8, 	4 * 8, 		1 * 8};
-const Size collision_7_2_1 = 	{7 * 8, 	2 * 8, 		1 * 8};
-const Size collision_8_3_1 = 	{8 * 8, 	3 * 8, 		1 * 8};
-const Size collision_8_4_1 = 	{8 * 8, 	4 * 8, 		1 * 8};
-const Size collision_8_20_1 = 	{8 * 8, 	20 * 8, 	1 * 8};
-const Size collision_12_4_1 = 	{12 * 8, 	4 * 8, 		1 * 8};
-const Size collision_12_12_1 = {12 * 8, 	12 * 8, 	1 * 8};
-const Size collision_14_6_1 = 	{14 * 8, 	6 * 8, 		1 * 8};
-const Size collision_14_18_1 = {14 * 8, 	18 * 8, 	1 * 8};
-const Size collision_16_2_1 = 	{16 * 8, 	2 * 8, 		1 * 8};
-const Size collision_18_6_1 = 	{18 * 8, 	6 * 8, 		1 * 8};
-const Size collision_20_2_1 = 	{20 * 8, 	2 * 8, 		1 * 8};
-const Size collision_20_6_1 = 	{20 * 8, 	6 * 8, 		1 * 8};
-const Size collision_20_32_1 = {20 * 8, 	32 * 8, 	1 * 8};
-const Size collision_28_8_1 = 	{28 * 8, 	8 * 8, 		1 * 8};
-const Size collision_28_16_1 = {28 * 8, 	16 * 8, 	1 * 8};
-const Size collision_32_14_1 = {32 * 8, 	14 * 8, 	1 * 8};
-const Size collision_32_4_1 = 	{32 * 8, 	4 * 8, 		1 * 8};
-const Size collision_30_33_1 = {30 * 8, 	33 * 8, 	1 * 8};
-const Size collision_34_30_1 = {34 * 8, 	30 * 8, 	1 * 8};
-const Size collision_38_10_1 = {38 * 8, 	10 * 8, 	1 * 8};
-const Size collision_42_18_1 = {42 * 8, 	18 * 8, 	1 * 8};
-const Size collision_47_3_1 = 	{47 * 8, 	3 * 8, 		1 * 8};
-const Size collision_48_2_1 = 	{48 * 8, 	2 * 8, 		1 * 8};
-const Size collision_48_3_1 = 	{48 * 8, 	3 * 8, 		1 * 8};
-const Size collision_48_4_1 = 	{48 * 8, 	4 * 8, 		1 * 8};
-const Size collision_48_10_1 = {48 * 8, 	10 * 8, 	1 * 8};
-const Size collision_48_18_1 = {48 * 8, 	18 * 8, 	1 * 8};
-const Size collision_48_28_1 = {48 * 8, 	28 * 8, 	1 * 8};
-const Size collision_49_5_1 = 	{49 * 8, 	5 * 8, 		1 * 8};
-const Size collision_67_25_1 = {67 * 8, 	25 * 8, 	1 * 8};
+const Size collision_2_28_1 = 	{2 * 8, 	28 * 8, 	8 * 8};
+const Size collision_2_64_1 = 	{2 * 8, 	64 * 8, 	8 * 8};
+const Size collision_3_2_1 = 	{3 * 8, 	2 * 8, 		8 * 8};
+const Size collision_4_2_1 = 	{4 * 8, 	2 * 8, 		8 * 8};
+const Size collision_4_4_1 = 	{4 * 8, 	4 * 8, 		8 * 8};
+const Size collision_4_8_1 = 	{4 * 8, 	8 * 8, 		8 * 8};
+const Size collision_6_4_1 = 	{6 * 8, 	4 * 8, 		8 * 8};
+const Size collision_7_2_1 = 	{7 * 8, 	2 * 8, 		8 * 8};
+const Size collision_8_3_1 = 	{8 * 8, 	3 * 8, 		8 * 8};
+const Size collision_8_4_1 = 	{8 * 8, 	4 * 8, 		8 * 8};
+const Size collision_8_20_1 = 	{8 * 8, 	20 * 8, 	8 * 8};
+const Size collision_12_4_1 = 	{12 * 8, 	4 * 8, 		8 * 8};
+const Size collision_12_12_1 = {12 * 8, 	12 * 8, 	8 * 8};
+const Size collision_14_6_1 = 	{14 * 8, 	6 * 8, 		8 * 8};
+const Size collision_14_18_1 = {14 * 8, 	18 * 8, 	8 * 8};
+const Size collision_16_2_1 = 	{16 * 8, 	2 * 8, 		8 * 8};
+const Size collision_18_6_1 = 	{18 * 8, 	6 * 8, 		8 * 8};
+const Size collision_20_2_1 = 	{20 * 8, 	2 * 8, 		8 * 8};
+const Size collision_20_6_1 = 	{20 * 8, 	6 * 8, 		8 * 8};
+const Size collision_20_32_1 = {20 * 8, 	32 * 8, 	8 * 8};
+const Size collision_28_8_1 = 	{28 * 8, 	8 * 8, 		8 * 8};
+const Size collision_28_16_1 = {28 * 8, 	16 * 8, 	8 * 8};
+const Size collision_32_14_1 = {32 * 8, 	14 * 8, 	8 * 8};
+const Size collision_32_4_1 = 	{32 * 8, 	4 * 8, 		8 * 8};
+const Size collision_30_33_1 = {30 * 8, 	33 * 8, 	8 * 8};
+const Size collision_34_30_1 = {34 * 8, 	30 * 8, 	8 * 8};
+const Size collision_38_10_1 = {38 * 8, 	10 * 8, 	8 * 8};
+const Size collision_42_18_1 = {42 * 8, 	18 * 8, 	8 * 8};
+const Size collision_47_3_1 = 	{47 * 8, 	3 * 8, 		8 * 8};
+const Size collision_48_2_1 = 	{48 * 8, 	2 * 8, 		8 * 8};
+const Size collision_48_3_1 = 	{48 * 8, 	3 * 8, 		8 * 8};
+const Size collision_48_4_1 = 	{48 * 8, 	4 * 8, 		8 * 8};
+const Size collision_48_10_1 = {48 * 8, 	10 * 8, 	8 * 8};
+const Size collision_48_18_1 = {48 * 8, 	18 * 8, 	8 * 8};
+const Size collision_48_28_1 = {48 * 8, 	28 * 8, 	8 * 8};
+const Size collision_49_5_1 = 	{49 * 8, 	5 * 8, 		8 * 8};
+const Size collision_67_25_1 = {67 * 8, 	25 * 8, 	8 * 8};
 
 
 //---------------------------------------------------------------------------------------------------------
@@ -134,7 +134,7 @@ void Collision_initialTransform(Collision this, Transformation* environmentTrans
 			this->size,
 
 			// displacement modifier
-			{0, 0, 0},
+			{0, 0, -__I_TO_FIX19_13(4 * 8)},
 
 			// rotation modifier
 			{0, 0, 0},
