2fd191466baf92e897500db68a470eaf73679d02
Jorge Eremiev <jorgech3@gmail.com>
2018-01-18 20:16:40 -0500

    Tweaked parallax displacement.

--- assets/images/Snail/Definition/SnailDefinition.c
+++ assets/images/Snail/Definition/SnailDefinition.c
@@ -130,7 +130,7 @@ ObjectSpriteROMDef SNAIL_SPRITE =
 		false,
 
 		// displacement
-		{0, 0, 0, 0},
+		{0, 0, 0, ENEMIES_PARALLAX_DISPLACEMENT},
 	},
 
 	// bgmap mode (__WORLD_BGMAP, __WORLD_AFFINE, __WORLD_OBJECT or __WORLD_HBIAS)
