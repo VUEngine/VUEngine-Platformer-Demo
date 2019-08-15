ff4a179201f7993b14fd44a14c4fd30bbcc4aed2
Christian Radke <christian.radke@unicepta.com>
2019-08-15 15:22:29 +0200

    Added autogenerated gameEvents file

---
+++ source/gameEvents.h
@@ -0,0 +1,37 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                              THIS FILE WAS AUTO-GENERATED - DO NOT EDIT                               //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef GAME_EVENTS_H_
+#define GAME_EVENTS_H_
+
+
+//---------------------------------------------------------------------------------------------------------
+//												INCLUDES
+//---------------------------------------------------------------------------------------------------------
+
+#include <Events.h>
+
+
+//---------------------------------------------------------------------------------------------------------
+//											 DEFINITIONS
+//---------------------------------------------------------------------------------------------------------
+
+
+enum GameEvents
+{
+	kEventCheckpointLoaded = kLastEngineEvent + 1,
+	kEventCoinTaken,
+	kEventHeroDied,
+	kEventHitTaken,
+	kEventKeyTaken,
+	kEventLevelCompleted,
+	kEventLevelStarted,
+	kEventPowerUp,
+	kEventScreenFocused,
+	kEventShakeCompleted,
+	kEventUserInput,
+};
+
+
+#endif
