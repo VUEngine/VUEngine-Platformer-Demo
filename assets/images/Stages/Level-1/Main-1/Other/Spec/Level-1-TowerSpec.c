5a23cffd775f9b78a23266d55d74fdedfb7a744e
Christian Radke <christian.radke@unicepta.com>
2019-11-08 11:19:48 +0100

    Fixed some charset definitions

--- assets/images/Stages/Level-1/Main-1/Other/Spec/Level-1-TowerSpec.c
+++ assets/images/Stages/Level-1/Main-1/Other/Spec/Level-1-TowerSpec.c
@@ -45,7 +45,7 @@ CharSetROMSpec LEVEL_1_TOWER_CH =
 	// number of chars, depending on allocation type:
 	// __ANIMATED_SINGLE*, __ANIMATED_SHARED*: number of chars of a single animation frame (cols * rows)
 	// __ANIMATED_MULTI, __NOT_ANIMATED: sum of all chars
-	28,
+	29,
 
 	// allocation type
 	// (__ANIMATED_SINGLE, __ANIMATED_SINGLE_OPTIMIZED, __ANIMATED_SHARED, __ANIMATED_SHARED_COORDINATED, __ANIMATED_MULTI or __NOT_ANIMATED)
@@ -122,7 +122,7 @@ EntityROMSpec LEVEL_1_TOWER_IM =
 	// class allocator
 	__TYPE(Entity),
 
-	// behaviors 
+	// behaviors
 	NULL,
 
 	// sprites
--- assets/images/Stages/Level-1/Tower/Back/Spec/Level-1-Tower-Back-1Spec.c
+++ assets/images/Stages/Level-1/Tower/Back/Spec/Level-1-Tower-Back-1Spec.c
@@ -47,7 +47,7 @@ CharSetROMSpec LEVEL_1_TOWER_BACK_1_CH =
 	// number of chars, depending on allocation type:
 	// __ANIMATED_SINGLE*, __ANIMATED_SHARED*: number of chars of a single animation frame (cols * rows)
 	// __ANIMATED_MULTI, __NOT_ANIMATED: sum of all chars
-	105,
+	79,
 
 	// allocation type
 	// (__ANIMATED_SINGLE, __ANIMATED_SINGLE_OPTIMIZED, __ANIMATED_SHARED, __ANIMATED_SHARED_COORDINATED, __ANIMATED_MULTI or __NOT_ANIMATED)
@@ -210,7 +210,7 @@ EntityROMSpec LEVEL_1_TOWER_MAIN_BACK_IM =
 	// class allocator
 	__TYPE(Entity),
 
-	// behaviors 
+	// behaviors
 	NULL,
 
 	// sprites
