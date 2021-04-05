5008506f856a3a041c7d6e6bcf4c7ea0cb0dec30
jorgeche <jorgech3@gmail.com>
2021-04-04 21:00:35 -0500

    Fixed compilation error.

--- assets/levels/Level1Spec.c
+++ assets/levels/Level1Spec.c
@@ -42,7 +42,7 @@ extern StageEntryPointROMSpec LEVEL_1_MAIN_MAIN_EP;
 PlatformerLevelROMSpec LEVEL_1_LV =
 {
 	// starting entry point
-	(StageEntryPointSpec*)&LEVEL_1_MAIN_TOWER_EP,
+	(StageEntryPointSpec*)&LEVEL_1_MAIN_MAIN_EP,
 
 	// id
 	1,
