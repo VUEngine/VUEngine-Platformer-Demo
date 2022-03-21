015ac8b53fd5d10cdd7b7a0623dca0ee662e7fbe
KR155E <c.radke@posteo.de>
2022-03-21 19:41:48 +0100

    Fix char counts

--- assets/images/Door/Spec/DoorSpec.c
+++ assets/images/Door/Spec/DoorSpec.c
@@ -189,7 +189,7 @@ CharSetROMSpec DoorCharset =
 	// number of chars, depending on allocation type:
 	// __ANIMATED_SINGLE*, __ANIMATED_SHARED*: number of chars of a single animation frame (cols * rows)
 	// __ANIMATED_MULTI, __NOT_ANIMATED: sum of all chars
-	85,
+	84,
 
 	// allocation type
 	// (__ANIMATED_SINGLE, __ANIMATED_SINGLE_OPTIMIZED, __ANIMATED_SHARED, __ANIMATED_SHARED_COORDINATED, __ANIMATED_MULTI or __NOT_ANIMATED)
@@ -550,7 +550,7 @@ CharSetROMSpec DoorBackCharset =
 	// number of chars, depending on allocation type:
 	// __ANIMATED_SINGLE*, __ANIMATED_SHARED*: number of chars of a single animation frame (cols * rows)
 	// __ANIMATED_MULTI, __NOT_ANIMATED: sum of all chars
-	25,
+	42,
 
 	// allocation type
 	// (__ANIMATED_SINGLE, __ANIMATED_SINGLE_OPTIMIZED, __ANIMATED_SHARED, __ANIMATED_SHARED_COORDINATED, __ANIMATED_MULTI or __NOT_ANIMATED)
